59837a40143707b3575a583bdbe6017a9865a657
Majd Saoud Al-harrbi <138905181+Majd80Saoud@users.noreply.github.com>
2025-07-20 20:48:09 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 $servername = "localhost";
-$username = "root";        // عدّله إذا كان مختلفًا
-$password = "";            // عدّله إذا فيه كلمة مرور
-$database = "all";         // اسم قاعدة البيانات
+$username = "root";        
+$password = "";            
+$database = "all";         
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
-// التحقق من الاتصال
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
