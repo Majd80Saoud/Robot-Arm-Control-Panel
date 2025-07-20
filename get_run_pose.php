5c02eb5af0d0106ef3af23e49e0984cc03633342
Majd Saoud Al-harrbi <138905181+Majd80Saoud@users.noreply.github.com>
2025-07-20 20:49:31 +0300

    Update get_run_pose.php

--- get_run_pose.php
+++ get_run_pose.php
@@ -1,16 +1,16 @@
 <?php
-include "db.php"; // تأكد أن ملف الاتصال بقاعدة البيانات موجود ومضبوط
+include "db.php"; 
 
-// جلب الوضعيات التي لم يتم حذفها (status = 1)
+
 $query = "SELECT * FROM pose WHERE status = 1";
 $result = mysqli_query($conn, $query);
 
 $poses = [];
 
 while ($row = mysqli_fetch_assoc($result)) {
-  $poses[] = $row; // يحتوي على ID و servo1 إلى servo6 و status
+  $poses[] = $row; 
 }
 
-// إعادة البيانات بصيغة JSON
+
 echo json_encode($poses);
 ?>
